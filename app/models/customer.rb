760c483326e77a4569d36cc17dac114a38189aea
Vinay Gupta <vinak@kreatio.com>
2015-03-02 13:24:43 +0530

    remove validation

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,7 +1,7 @@
 class Customer < ActiveRecord::Base
-  validates :name, presence: true
-  validates :city, presence: true
-  validates :address, presence: true
+  # validates :name, presence: true
+  # validates :city, presence: true
+  # validates :address, presence: true
   has_many :orders , :dependent => :destroy
 
 end
