60c8a78631e375b0d1c2a9543c5ccd6620b15e40
Vinay Gupta <vinak@kreatio.com>
2015-03-03 17:42:22 +0530

    edit in unicorn file

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,20 +1,20 @@
 worker_processes 2
-working_directory "/home/vinay/Desktop/Example/modeldemo" # available in 0.94.0+
+working_directory "/var/www/model_demo/current"
 listen 4000, :tcp_nopush => true
 timeout 120
-pid "/home/vinay/Desktop/Example/modeldemo/tmp/pids/unicorn.pid"
+pid "/var/www/model_demo/current/tmp/pids/unicorn.pid"
 preload_app true
 GC.respond_to?(:copy_on_write_friendly=) and
-  GC.copy_on_write_friendly = true
+    GC.copy_on_write_friendly = true
 
 before_fork do |server, worker|
-    defined?(ActiveRecord::Base) and
-        ActiveRecord::Base.connection.disconnect!
+  defined?(ActiveRecord::Base) and
+      ActiveRecord::Base.connection.disconnect!
 end
 after_fork do |server, worker|
-    defined?(ActiveRecord::Base) and
-        ActiveRecord::Base.establish_connection
+  defined?(ActiveRecord::Base) and
+      ActiveRecord::Base.establish_connection
 end
 
-stderr_path "/home/vinay/Desktop/Example/modeldemo/log/unicorn_err.log"
-stdout_path "/home/vinay/Desktop/Example/modeldemo/log/unicorn_out.log"
+stderr_path "/var/www/model_demo/current/log/unicorn_err.log"
+stdout_path "/var/www/model_demo/current/log/unicorn_out.log"
