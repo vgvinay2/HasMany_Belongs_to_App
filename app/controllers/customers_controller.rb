590731570316e79c159fdfbba289896cd42f3b1f
Vinay Gupta <vgvinay2@gmail.com>
2016-01-13 17:14:28 +0530

    removed commented code

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,35 +1,27 @@
 class CustomersController < ApplicationController
+
   before_action :set_customer, only: [:show, :edit, :update, :destroy]
-  # GET /customers
-  # GET /customers.json
+
   def index
     @customers = Customer.search(params[:keyword])
   end
 
-  # GET /customers/1
-  # GET /customers/1.json
   def show
   end
 
-  # GET /customers/new
   def new
     @customer = Customer.new
   end
 
-  # GET /customers/1/edit
   def edit
   end
 
-  # POST /customers
-  # POST /customers.json
   def create
     @customer = Customer.new(customer_params)
     @customer.save
     redirect_to @customer
   end
 
-  # PATCH/PUT /customers/1
-  # PATCH/PUT /customers/1.json
   def update
     respond_to do |format|
       if @customer.update(customer_params)
@@ -42,8 +34,6 @@ class CustomersController < ApplicationController
     end
   end
 
-  # DELETE /customers/1
-  # DELETE /customers/1.json
   def destroy
     @customer.destroy
     respond_to do |format|
@@ -53,14 +43,12 @@ class CustomersController < ApplicationController
   end
 
   private
-  # Use callbacks to share common setup or constraints between actions.
   def set_customer
     @customer = Customer.find(params[:id])
   end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
-
   def customer_params
     params.required(:customer).permit(:name, :address, :city, :status)
   end
+
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,33 +1,26 @@
 class OrdersController < ApplicationController
+
   before_action :set_order, only: [:show, :edit, :update, :destroy]
 
-  # GET /orders
-  # GET /orders.json
   def index
     @customer = Customer.find(params[:customer_id])
     @orders = Order.all
   end
 
-  # GET /orders/1
-  # GET /orders/1.json
   def show
     @customer = Customer.find(params[:customer_id])
     @order = Order.find(params[:id])
   end
 
-  # GET /orders/new
   def new
     @customer = Customer.find(params[:customer_id])
     @order = @customer.orders.build
   end
 
-  # GET /orders/1/edit
   def edit
     @customer = Customer.find(params[:customer_id])
   end
 
-  # POST /orders
-  # POST /orders.json
   def create
     @customer = Customer.find(params[:customer_id])
     @order = @customer.orders.build(order_params)
@@ -35,22 +28,16 @@ class OrdersController < ApplicationController
     redirect_to customer_order_path(@customer.id, @order.id)
   end
 
-  # PATCH/PUT /orders/1
-  # PATCH/PUT /orders/1.json
   def update
     respond_to do ormatat|
       if @order.update(order_params)
        ormatat.html { redirect_to @order, notice: 'Order was successfully updated.' }
-       ormatat.json { head :no_content }
       else
        ormatat.html { render action: 'edit' }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /orders/1
-  # DELETE /orders/1.json
   def destroy
     @order.destroy
     respond_to do |format|
@@ -60,7 +47,7 @@ class OrdersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+
     def set_order
       @order = Order.find(params[:id])
     end
